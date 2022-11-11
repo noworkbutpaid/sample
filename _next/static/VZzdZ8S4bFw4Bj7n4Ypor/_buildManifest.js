e7b0d7733f294b49b3bf0edf89fd032c5eb49c61
Omaewa <35822384+dprequim@users.noreply.github.com>
2022-11-11 11:19:59 -0800

    Add files via upload

---
+++ _next/static/VZzdZ8S4bFw4Bj7n4Ypor/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/65-c634d85919aae10e.js","static/css/46ed9ad775c70fb6.html","static/chunks/pages/index-29694c3433133cbd.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
